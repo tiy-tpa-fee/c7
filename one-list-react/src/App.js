5976f6a7d8ec3e20e9c6c56d432b12e2d2fb2ba1
Jason L Perry <jasper@ambethia.com>
2017-04-05 12:24:01 -0400

    One list

--- one-list-react/src/App.js
+++ one-list-react/src/App.js
@@ -1,35 +1,54 @@
-import React, { Component } from 'react';
-import Header from './Header'
-import Intro from './Intro'
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return <div className="App">
-      <Header />
-      <Intro />
-    </div>
+class App extends React.Component {
+
+  state = {
+    plants: []
   }
 
-  // render() {
-  //   let heading
-  //
-  //   if (false) {
-  //     heading = React.createElement('h2', null, "Welcome to React")
-  //   } else {
-  //     heading = React.createElement('h2', null, "Gross React")
-  //   }
-  //
-  //   return (
-  //     React.createElement('div', { className: 'App' }, [
-  //       React.createElement('div', { className: 'App-header' }, [
-  //         React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
-  //         heading
-  //       ]),
-  //       React.createElement('p', { className: 'App-intro'}, "To get started, edit src/App.js and save to reload.")
-  //     ])
-  //   );
-  // }
+  _submit = (event) => {
+    event.preventDefault()
+    this.setState({
+      plants: [...this.state.plants, {
+        label: this.refs.todoText.value,
+        done: false
+      }]
+    })
+    this.refs.todoText.value = ''
+  }
+
+  markComplete (index) {
+    const newItems = this.state.plants.slice()
+    newItems[index].done = !newItems[index].done
+    this.setState({ plants: newItems })
+  }
+
+  render () {
+
+    const plantItems = this.state.plants.map((plant, i) => {
+      return <li
+        onClick={() => this.markComplete(i)}
+        className={plant.done ? 'completed' : ''}
+        key={i}>
+        {plant.label}
+      </li>
+    })
+
+    return <div className="App">
+      <header>
+        <h1>One List</h1>
+      </header>
+      <main>
+        <ul className="one-list">
+          {plantItems}
+        </ul>
+        <form onSubmit={this._submit}>
+          <input type="text" ref="todoText" />
+        </form>
+      </main>
+      <footer>&copy; 2017 Cohort Seven.</footer>
+    </div>
+  }
 }
 
-export default App;
+export default App
